389a04ce7eeaf746ae73213bf8c312303ca241d7
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-06-04 12:23:27 +0530

    add routes to screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+// Screens
+import Home from "./screens/Home";
+import Learn from "./screens/Learn";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import Settings from "./screens/Settings";
+
+// Components
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="home" element={<Home />} />
+          <Route path="learn" element={<Learn />} />
+          <Route path="settings" element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
