312ae087acbf74fc7d78fc381efdc50c57cc7e55
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-06-15 10:51:17 +0530

    use cell component on test screen

--- src/screens/Test/index.tsx
+++ src/screens/Test/index.tsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 
 // Components
 import Prediction from "../../components/Prediction";
+import Cell from "../../components/Cell";
 
 // TODO: Remove screen after testing
 const Test = () => {
   const [isRecording, setIsRecording] = useState(false);
   const [predictedLetter, setPredictedLetter] = useState("");
+  const [presentCell, setPresentCell] = useState(0);
+  const [word, setWord] = useState("PRIMO");
+  const [isCheck, setIsCheck] = useState(false);
 
   useEffect(() => {
     console.log("isRecording", isRecording);
@@ -15,12 +19,66 @@ const Test = () => {
 
   return (
     <>
-      <Prediction
+      {/* <Prediction
         isRecording={isRecording}
         predictedLetter={predictedLetter}
         setPredictedLetter={setPredictedLetter}
-      />
-      <button onClick={() => setIsRecording(!isRecording)}>Press</button>
+      /> */}
+      {/* <button onClick={() => setIsRecording(!isRecording)}>Press</button> */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          margin: "25px 0px",
+        }}
+      >
+        <Cell
+          presentCell={presentCell}
+          cellNumber={0}
+          cellLetter="P"
+          // predictedLetter={predictedLetter}
+          predictedLetter="P"
+          word={word}
+          isCheck={isCheck}
+        />
+        <Cell
+          presentCell={presentCell}
+          cellNumber={1}
+          cellLetter="R"
+          // predictedLetter={predictedLetter}
+          predictedLetter="R"
+          word={word}
+          isCheck={isCheck}
+        />
+        <Cell
+          presentCell={presentCell}
+          cellNumber={2}
+          cellLetter="I"
+          // predictedLetter={predictedLetter}
+          predictedLetter="I"
+          word={word}
+          isCheck={isCheck}
+        />
+        <Cell
+          presentCell={presentCell}
+          cellNumber={3}
+          cellLetter="M"
+          // predictedLetter={predictedLetter}
+          predictedLetter="M"
+          word={word}
+          isCheck={isCheck}
+        />
+        <Cell
+          presentCell={presentCell}
+          cellNumber={4}
+          cellLetter="O"
+          // predictedLetter={predictedLetter}
+          predictedLetter="O"
+          word={word}
+          isCheck={isCheck}
+        />
+      </div>
+      <button onClick={() => setIsCheck(!isCheck)}>Check</button>
     </>
   );
 };
