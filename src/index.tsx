c72b7f501ca5801d2fc7c0113eacb2ea543989a4
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-06-20 10:45:47 +0530

    setup chakra ui, react icons and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Roboto Slab', serif`,
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
