d2cff37372791572e0ff9380b447aa1201e3794c
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-06-20 10:46:39 +0530

    design header component ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import { Box, Flex, Heading, IconButton, Spacer } from "@chakra-ui/react";
+import {
+  AiOutlineQuestionCircle,
+  AiOutlineSetting,
+  AiOutlineUser,
+} from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={2}
+      bg="white.500"
+      color="blackAlpha.900"
+      borderBottom="1px solid #CBD5E0"
+    >
+      <Box>
+        <IconButton
+          aria-label="How to play"
+          icon={<AiOutlineQuestionCircle />}
+        />
+      </Box>
+      <Spacer />
+      <Box>
+        <Heading as="h2" size="xl">
+          Signle
+        </Heading>
+      </Box>
 
-export default Header
+      <Spacer />
+      <Box>
+        <IconButton aria-label="Profile" icon={<AiOutlineUser />} />
+        <IconButton aria-label="Settings" icon={<AiOutlineSetting />} />
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
