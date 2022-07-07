a271f0efbd8c3e9495af9531d53cf008252ae3cf
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-07-07 23:13:56 +0530

    implement settings screen

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Box,
+  Container,
+  Divider,
+  Flex,
+  HStack,
+  IconButton,
+  Switch,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { AiOutlineRight } from "react-icons/ai";
 
 const Settings = () => {
   return (
-    <div>Settings</div>
-  )
-}
+    <Flex
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      bg="white.500"
+      p={3}
+    >
+      <Container maxW="2xl" centerContent>
+        <Box w="85%">
+          <Text fontSize="xl" paddingY={5}>
+            Settings
+          </Text>
+          <VStack>
+            <HStack w="100%">
+              <VStack w="80%" spacing={0} alignItems={"start"}>
+                <Text fontSize="lg" paddingBottom={0} align={"left"}>
+                  Easy Mode
+                </Text>
+                <Text
+                  fontSize="sm"
+                  color={"gray.500"}
+                  paddingBottom={3}
+                  align={"left"}
+                >
+                  Word check will be disabled
+                </Text>
+              </VStack>
+              <Switch size="md" />
+            </HStack>
+            <Divider marginY={5} />
+            <HStack w="100%">
+              <VStack w="80%" spacing={0} alignItems={"start"}>
+                <Text fontSize="lg" paddingBottom={0} align={"left"}>
+                  Dark Mode
+                </Text>
+                <Text
+                  fontSize="sm"
+                  color={"gray.500"}
+                  paddingBottom={3}
+                  align={"left"}
+                >
+                  Coming soon...
+                </Text>
+              </VStack>
+              <Switch size="md" />
+            </HStack>
+            <Divider marginY={5} />
+            <HStack w="100%">
+              <VStack w="80%" spacing={0} alignItems={"start"}>
+                <Text fontSize="lg" paddingY={3} align={"left"}>
+                  Edit Profile
+                </Text>
+              </VStack>
+              <IconButton
+                aria-label="Edit profile"
+                icon={<AiOutlineRight />}
+                backgroundColor="white"
+              />
+            </HStack>
+            <Divider marginY={5} />
+            <HStack w="100%">
+              <VStack w="70%" spacing={0} alignItems={"start"}>
+                <Text fontSize="lg" paddingBottom={0} align={"left"}>
+                  Feedback
+                </Text>
+                <Text
+                  fontSize="sm"
+                  color={"gray.500"}
+                  paddingBottom={3}
+                  align={"left"}
+                >
+                  Please consider evaluating project
+                </Text>
+              </VStack>
+              <Text fontSize="sm" paddingBottom={3} align={"left"}>
+                Google Forms
+              </Text>
+            </HStack>
+            <Divider marginY={5} />
+            <HStack w="100%">
+              <VStack w="80%" spacing={0} alignItems={"start"}>
+                <Text fontSize="lg" paddingY={3} align={"left"}>
+                  Questions?
+                </Text>
+              </VStack>
+              <Text fontSize="sm" paddingBottom={3} align={"left"}>
+                Email
+              </Text>
+            </HStack>
+            <Divider marginY={5} />
+          </VStack>
+        </Box>
+      </Container>
+    </Flex>
+  );
+};
 
-export default Settings
+export default Settings;
