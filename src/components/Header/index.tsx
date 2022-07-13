739d59b8b1f1be8d7c16062868b11c649ae313ff
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-07-13 23:13:03 +0530

    show reference modal from button in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 
 import {
   Box,
+  Button,
   ButtonGroup,
+  Divider,
   Flex,
   Heading,
   IconButton,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spacer,
+  useDisclosure,
+  Text,
+  Image,
+  Link,
 } from "@chakra-ui/react";
 import {
   AiOutlineHome,
@@ -16,8 +28,13 @@ import {
   AiOutlineUser,
 } from "react-icons/ai";
 
+// Assets
+import ASLAlphabets from "../../assets/alphabets/ASL-Alphabets.jpg";
+
 const Header = () => {
   const navigate = useNavigate();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Flex
@@ -30,6 +47,51 @@ const Header = () => {
       color="blackAlpha.900"
       borderBottom="1px solid #CBD5E0"
     >
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        size={"lg"}
+      >
+        <ModalOverlay
+          bg="blackAlpha.300"
+          backdropFilter="blur(10px) hue-rotate(90deg)"
+        />
+        <ModalContent>
+          <ModalHeader>Reference</ModalHeader>
+          <Divider />
+          <ModalBody>
+            <Text
+              fontSize="sm"
+              color={"blackAlpha.900"}
+              paddingBottom={3}
+              align={"left"}
+            >
+              Use the following reference whenever you are stuck. It is
+              recommended that you access the{" "}
+              <Link
+                as={RouterLink}
+                to="/learn"
+                color="blue.500"
+                onClick={onClose}
+              >
+                Learn
+              </Link>{" "}
+              page to train and get familiar with the app for the best result
+            </Text>
+            <Flex w="100%" paddingX={"10"}>
+              <Image src={ASLAlphabets} alt={"ASL Alphabets"} />
+            </Flex>
+          </ModalBody>
+          <Divider />
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       <Box>
         <ButtonGroup gap="2">
           <IconButton
@@ -40,8 +102,9 @@ const Header = () => {
             }}
           />
           <IconButton
-            aria-label="How to play"
+            aria-label="Reference"
             icon={<AiOutlineQuestionCircle />}
+            onClick={onOpen}
           />
         </ButtonGroup>
       </Box>
